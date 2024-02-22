83648f273dc8d65e53ce3d38e558c8ee18084e47
Md. Rashed Miah <rashedm@nextsolutionlab.com>
2024-02-22 17:58:44 +0600

    first commit

---
+++ Big O/BigO-3NestedLoops.js
@@ -0,0 +1,11 @@
+function logItems(n) {
+     for(let i = 0; i < n; i++) {
+         for(let j = 0; j < n; j++) {
+             for(let k = 0; k < n; k++) {
+                 console.log(i, j, k) 
+             }       
+         }       
+     } 
+ }
+ 
+ logItems(10)
---
+++ Big O/BigO-TwoLoops.js
@@ -0,0 +1,11 @@
+function logItems(n) {
+     for(let i = 0; i < n; i++) {
+        console.log(i) 
+     }
+ 
+     for(let j = 0; j < n; j++) {
+        console.log(j) 
+     }       
+ }
+ 
+ logItems(3)
